627005a94e83b0beff86c0a0f95b1bcd9d141495
mylcd <yinlongmi@gmail.com>
2024-06-10 17:13:50 -0700

    Frontend Framework Setted Up

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -52,8 +52,8 @@ router.use('/spot-images', spotImagesRouter);
 
 router.use('/review-images', reviewImagesRouter);
 
-router.post('/test', function(req, res) {
+/*router.post('/test', function(req, res) {
   res.json({ requestBody: req.body });
-});
+});*/
 
 module.exports = router;
