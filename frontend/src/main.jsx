3ba481bce46e849e3de93b72a84c5be5766d4b93
mylcd <yinlongmi@gmail.com>
2024-06-12 17:09:41 -0700

    Modal for Login and Signup Added

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,9 +4,9 @@ import App from './App';
 import './index.css';
 import { Provider } from 'react-redux';
 import configureStore from './store/store';
-
 import { restoreCSRF, csrfFetch } from './store/csrf';
 import * as sessionActions from './store/session';
+import { Modal, ModalProvider } from './context/Modal';
 
 const store = configureStore();
 
@@ -20,8 +20,11 @@ if (import.meta.env.MODE !== 'production') {
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <ModalProvider>
+      <Provider store={store}>
+        <App />
+        <Modal />
+      </Provider>
+    </ModalProvider>
   </React.StrictMode>
 );
